1c2407e58acc68a38b3a6eea160cb5b14c03566f
CD1974 <mjcd1973@gmail.com>
2025-06-27 20:05:23 +0200

    1st displays

---
+++ .github/workflows/01_01/app_titulo.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df_ventas = pd.DataFrame({
+        "annio": [2019, 2020, 2021, 2022, 2023, 2024],
+        "venta_cromos_101": [119.25, 141.28, 132.06, 205.33, 216.59, 157.51],
+        "venta_cromos_102": [51.15, 90.39, 43.09, 178.21, 211.79, 450.28]
+    })
+
+print(df_ventas)
+
+fig, ax = plt.subplots()
+
+ax.plot(df_ventas['annio'], df_ventas['venta_cromos_101'])
+ax.plot(df_ventas['annio'], df_ventas['venta_cromos_102'])
+plt.title('Ventas en dólares de cromos del 2019 al 2024', fontsize=14, fontweight='bold', color='blue', ha='center', pad=20)
+plt.show()
