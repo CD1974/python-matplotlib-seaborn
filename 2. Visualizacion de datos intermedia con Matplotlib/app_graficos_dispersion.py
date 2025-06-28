ac7824c2266898eab06761874018f21c62f7acd8
CD1974 <mjcd1973@gmail.com>
2025-06-28 19:39:44 +0200

    update graficos circulares y dispersión

---
+++ 2. Visualizacion de datos intermedia con Matplotlib/app_graficos_dispersion.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df_empleados = pd.read_csv('empleados.csv', delimiter=";")
+
+fig, ax = plt.subplots()
+
+ax.scatter(df_empleados['Experiencia'], df_empleados['Salario'], color='gray')
+
+ax.set_xlabel('Experiencia Laboral (años)')
+ax.set_ylabel('Salario (Dólares)')
+ax.set_title('Experiencia Laboral vs Salario', fontsize=16, color='olivedrab', fontweight='bold', pad=20)
+plt.show()
